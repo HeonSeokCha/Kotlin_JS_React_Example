98f9ce45cfb53858215eeee7c41ff85cad1c8ce6
HyeonSeokCha <hs960829@gmail.com>
2022-06-04 01:40:41 +0900

    prepare KakakoMap Js
    add div tag in visual for map.

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,17 +1,26 @@
+
+import csstype.Properties
 import csstype.px
+import emotion.css.css
+import emotion.react.css
 import kotlinx.browser.document
-import react.Fragment
-import react.create
+import react.*
 import react.dom.client.createRoot
 import react.dom.html.ReactHTML.div
 
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
     val root = createRoot(container)
-
-    root.render(Fragment.create() {
-        div {
-            id = "map"
+    root.render(
+        Fragment.create {
+            div {
+                id = "map"
+                css {
+                    width = 500.px
+                    height = 400.px
+                }
+            }
         }
-    })
+    )
 }
+
--- src/main/kotlin/KakaoMap.kt
+++ src/main/kotlin/KakaoMap.kt
@@ -2,32 +2,18 @@
 
 import kotlinx.browser.document
 import org.w3c.dom.Element
+import org.w3c.dom.Window
 import react.*
 
-val Map = {
+
+val KMap = FC<Props> {
     useEffectOnce {
+
         var container = document.getElementById("map")
-        val options = Options.apply {
-            center = kakao.maps.LatLng(33.450701, 126.570667)
-            level = 3
-        }
-        kakao.maps.Map(container, options)
+//        options {
+//            center = kakao.maps.LatLng(33.450701, 126.570667)
+//            level = 3
+//        }
+//        kakao.maps.Map(container, TelegramIcon)
     }
 }
-
-
-external object Options {
-    var center: Any
-    var level: Int
-}
-
-external object kakao {
-    val maps: Maps
-}
-
-external interface Maps {
-    fun Map(container: Element?, options: Options)
-    fun LatLng(a: Double, b: Double)
-}
-
-
